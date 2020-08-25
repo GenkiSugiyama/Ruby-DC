edeb90112f6f68b9fe4838db3826cb2e3c7b2fe3
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-08-25 09:24:57 +0900

    [1-5完了]パラメータに任意の初期値を設定

--- main.rb
+++ main.rb
@@ -2,12 +2,14 @@ class Brave
 
   # initializeメソッドを定義
   # new演算子から引数を受け取り任意の初期値を設定する
-  # 引数にキーワード指定することでどのパラメーターにどういう値が入るか可読性をあげる
-  def initialize(name:, hp:, offense:, defense:)
-    @name = "テリー"
-    @hp = 500
-    @offense = 150
-    @defense = 100
+  # 引数にparamsを指定し一括で受け取る
+  # 引数に**をつけることで受け取れる引数をハッシュのみに限定する
+  def initialize(**params)
+    # paramsで受け取った値の中からハッシュで受け取る値を指定
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
   end
 
   # nameのゲッター
@@ -32,7 +34,7 @@ class Brave
 
 end
 
-brave = Brave.new(name: "テリー",hp: 500,offense: 150,defense: 100)
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 puts <<~TEXT
 NAME:#{brave.name}
 HP:#{brave.hp}
