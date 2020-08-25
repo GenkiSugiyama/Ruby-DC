12dd2520bc9fcbb868f36f6d808fac0c0e62520d
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-08-25 09:17:37 +0900

    [1-5途中]パラメータに任意の初期値を設定

--- main.rb
+++ main.rb
@@ -1,50 +1,29 @@
 class Brave
 
   # initializeメソッドを定義
-  # Object.newで新しいインスタンスを作った時にinitializeメソッドで定義された初期値をパラメーターに格納する
-  def initialize
+  # new演算子から引数を受け取り任意の初期値を設定する
+  # 引数にキーワード指定することでどのパラメーターにどういう値が入るか可読性をあげる
+  def initialize(name:, hp:, offense:, defense:)
     @name = "テリー"
     @hp = 500
     @offense = 150
     @defense = 100
   end
 
-  # このクラスが持つパラメータの設定をセッターメソッドを使って行う
-  # nameのセッター
-  # 関数名=(引数名) とするのはセッターだから
-  # def name=(name)
-  #   @name = name
-  # end
-
   # nameのゲッター
   def name
     @name
   end
-
-  # hpのセッター
-  # def hp=(hp)
-  #   @hp = hp
-  # end
 
   # hpのゲッター
   def hp
     @hp
   end
 
-  # offenseのセッター
-  # def offense=(offense)
-  #   @offense = offense
-  # end
-
   # offenseのゲッター
   def offense
     @offense
   end
-
-  # defenseのセッター
-  # def defense=(defense)
-  #   @defense = defense
-  # end
 
   # defenseのゲッター
   def defense
@@ -53,14 +32,7 @@ class Brave
 
 end
 
-# initializeメソッド定義したことにより不要となる
-# brave = Brave.new
-# brave.name = "テリー"
-# brave.hp = 500
-# brave.offense = 150
-# brave.defense = 100
-
-brave = Brave.new
+brave = Brave.new(name: "テリー",hp: 500,offense: 150,defense: 100)
 puts <<~TEXT
 NAME:#{brave.name}
 HP:#{brave.hp}
