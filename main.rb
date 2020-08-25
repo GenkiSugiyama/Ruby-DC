a4d7973a86a934034ba7c99c42e938fbeff54562
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-08-25 09:43:36 +0900

    [2章]モンスタークラスの定義

--- main.rb
+++ main.rb
@@ -22,6 +22,24 @@ class Brave
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+
+class Monster
+  # name, offense, defense属性は値の取り出し（ゲッター）のみ
+  attr_reader :name, :offense, :defense
+  # hpは取り出し、代入が可能
+  attr_accessor :hp
+
+  # new演算子の引数を受け取って初期値を設定
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defencse = params[:defencse]
+  end
+end
+
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+
 puts <<~TEXT
 NAME:#{brave.name}
 HP:#{brave.hp}
@@ -29,6 +47,13 @@ OFFENSE:#{brave.offense}
 DEFENSE:#{brave.defense}
 TEXT
 
+puts <<~TEXT
+NAME:#{monster.name}
+HP:#{monster.hp}
+OFFENSE:#{monster.offense}
+DEFENSE:#{monster.defense}
+TEXT
+
 brave.hp -= 30
 
-puts "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
+puts "#{brave.name}は#{monster.name}からダメージを受けた!　残りHPは#{brave.hp}だ"
