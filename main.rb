e4c5eab90afd5a9d54caaa25ac5f8890797c5c8b
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-08-27 09:42:28 +0900

    [3-1]勇者の攻撃処理を実装

--- main.rb
+++ main.rb
@@ -19,9 +19,20 @@ class Brave
     @defense = params[:defense]
   end
 
-end
+  # 攻撃処理を実装
+  # 引数に攻撃対象となるモンスタークラスのインスタンスを受け取る
+  def attack(monster)
+    # ダメージ計算：勇者の攻撃力 - モンスターの守備力
+    damage = @offense - monster.defense
+    # 攻撃対象のモンスターHPからダメージ分を引く
+    monster.hp -= damage
 
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+    puts "#{@name}の攻撃"
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+end
 
 class Monster
   # name, offense, defense属性は値の取り出し（ゲッター）のみ
@@ -34,26 +45,11 @@ class Monster
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
-    @defencse = params[:defencse]
+    @defense = params[:defense]
   end
 end
 
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-puts <<~TEXT
-NAME:#{brave.name}
-HP:#{brave.hp}
-OFFENSE:#{brave.offense}
-DEFENSE:#{brave.defense}
-TEXT
-
-puts <<~TEXT
-NAME:#{monster.name}
-HP:#{monster.hp}
-OFFENSE:#{monster.offense}
-DEFENSE:#{monster.defense}
-TEXT
-
-brave.hp -= 30
-
-puts "#{brave.name}は#{monster.name}からダメージを受けた!　残りHPは#{brave.hp}だ"
+brave.attack(monster)
