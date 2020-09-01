32fd0afe5e264db9124df4dc45c405866bd06c53
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-09-01 22:13:56 +0900

    [3-2]必殺攻撃処理の実装

--- main.rb
+++ main.rb
@@ -7,6 +7,9 @@ class Brave
   # 値の参照（ゲッター）と値の更新（セッター）をattr_accessorで同時に定義する
   attr_accessor :hp
 
+  # 必殺攻撃に使う倍率を定数として定義しておく
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   # initializeメソッドを定義
   # new演算子から引数を受け取り任意の初期値を設定する
   # 引数にparamsを指定し一括で受け取る
@@ -22,14 +25,32 @@ class Brave
   # 攻撃処理を実装
   # 引数に攻撃対象となるモンスタークラスのインスタンスを受け取る
   def attack(monster)
-    # ダメージ計算：勇者の攻撃力 - モンスターの守備力
-    damage = @offense - monster.defense
+    puts "#{@name}の攻撃"
+
+    # 必殺攻撃と通常攻撃の切り分け（4分の1の確率で必殺攻撃を行う）
+    # 0~3の間のランダムに数字が変わる
+    attack_num = rand(4)
+
+    # 4分の1の確率で必殺攻撃を行う
+    if attack_num == 0
+      # 必殺攻撃
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      # 通常攻撃（ダメージ計算：勇者の攻撃力 - モンスターの守備力）
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     # 攻撃対象のモンスターHPからダメージ分を引く
     monster.hp -= damage
 
-    puts "#{@name}の攻撃"
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
   end
 
 end
