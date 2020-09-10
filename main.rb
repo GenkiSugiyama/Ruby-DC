90584b3304f0fd77771653db634ad13b0d8ae8e9
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-09-10 09:43:38 +0900

    [4-4]main.rbのリファクタリング

--- main.rb
+++ main.rb
@@ -2,23 +2,11 @@
 # Characterクラスはこのファイルでは扱わないのでrequireしない
 require './brave'
 require './monster'
+require './games_controller'
+
+games_controller = GamesController.new
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-# loop doでループ処理
-loop do
-  brave.attack(monster)
-
-  # モンスターのHPが0になったらループ処理を終了する
-  if monster.hp <= 0
-    break
-  end
-
-  monster.attack(brave)
-
-  # 勇者のHPが0になったらループ処理を終了する
-  if brave.hp <= 0
-    break
-  end
-end
+games_controller.battle(brave: brave, monster: monster)
