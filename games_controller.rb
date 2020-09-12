7b16417db5890cac3e7fdacaedc5f96f31df4911
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-09-12 16:50:53 +0900

    [4-5]games_controller.rbのリファクタリング

--- games_controller.rb
+++ games_controller.rb
@@ -3,41 +3,53 @@ class GamesController
   GOLD_CONSTANT = 3
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
 
+    # 渡される勇者クラス、モンスタークラスをインスタンス変数に格納するbuild_characterメソッドを呼び出す
+    build_characters(params)
     # loop doでループ処理
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
+      @brave.attack(@monster)
+      break if battle_end?
 
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @monster.attack(@brave)
+      break if battle_end?
     end
 
-    if battle_result(brave)
-      result = caluclate_of_exp_and_gold(monster)
-      puts "#{brave.name}はたたかいに勝った"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
-    else
-      puts "#{brave.name}はたたかいに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    battle_judgment
+
   end
 
   private
 
-  def battle_end?(character)
-    character.hp <= 0
+  def build_characters(**params)
+    # インスタンス変数を定義しておくことで各メソッドで引数を取らず
+    # インスタンス変数を使って処理がかける
+    @brave = params[:brave]
+    @monster = params[:monster]
   end
 
-  def battle_result(brave)
-    brave.hp > 0
+  def battle_end?
+    @brave.hp <= 0 || @monster.hp <= 0
   end
 
-  def caluclate_of_exp_and_gold(monster)
-    exp = (monster.offense + monster.defense) * EXP_CONSTANT # 経験値の計算に使用する定数
-    gold = (monster.offense + monster.defense) * GOLD_CONSTANT # ゴールドの計算に使用する定数
+  def brave_win?
+    @brave.hp > 0
+  end
+
+  def battle_judgment
+    if brave_win?
+      result = caluclate_of_exp_and_gold
+      puts "#{@brave.name}はたたかいに勝った"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+    else
+      puts "#{@brave.name}はたたかいに負けた"
+      puts "目の前が真っ暗になった"
+    end
+  end
+
+  def caluclate_of_exp_and_gold
+    exp = (@monster.offense + @monster.defense) * EXP_CONSTANT # 経験値の計算に使用する定数
+    gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT # ゴールドの計算に使用する定数
     result = {exp: exp, gold: gold}
 
     result
